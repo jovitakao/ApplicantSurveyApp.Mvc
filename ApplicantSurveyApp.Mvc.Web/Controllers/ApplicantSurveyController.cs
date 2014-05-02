873ea2197e221446d947496190eeca6d3577e3ee
Joseph <seph0309@gmail.com>
2014-05-02 21:37:59 +0800

    Added ApplicantSurvey Controller and View

---
+++ ApplicantSurveyApp.Mvc.Web/Controllers/ApplicantSurveyController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ApplicantSurveyApp.Mvc.Web.Controllers
+{
+    public class ApplicantSurveyController : Controller
+    {
+        //
+        // GET: /ApplicantSurvey/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //
+        // GET: /ApplicantSurvey/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        //
+        // GET: /ApplicantSurvey/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /ApplicantSurvey/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /ApplicantSurvey/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /ApplicantSurvey/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /ApplicantSurvey/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /ApplicantSurvey/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
